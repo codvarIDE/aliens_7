4733d69cc95e79fce016e5c03c6ce5653808dad2
CobSphere <148908687+codvarIDE@users.noreply.github.com>
2024-11-21 06:16:21 -1200

    Delete example.py

--- example.py
+++
@@ -1,8 +0,0 @@
-import ankaramesi
-
-encrypted_data,keys = ankaramesi.encrypt_text_with_keys("Messi is the Goat")
-print("______________________________Encrypted data___________________________________________")
-print(encrypted_data)
-print("______________________________Keys_______________________________________________________")
-
-print(keys)
