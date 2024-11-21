874c40b817f457b61c7a521188b42dc43b06f1ef
CobSphere <148908687+codvarIDE@users.noreply.github.com>
2024-11-21 06:20:01 -1200

    Update encrypt.py

--- encrypt.py
+++ encrypt.py
@@ -1,1 +1,8 @@
+import aliens_7
 
+encrypted_data,keys = ankaramesi.encrypt_text_with_keys("Messi is the Goat")
+print("______________________________Encrypted data___________________________________________")
+print(encrypted_data)
+print("______________________________Keys_______________________________________________________")
+
+print(keys)
